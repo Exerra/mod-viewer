e17f168ee24ba8b82523d2f7eb0406919bc726a9
Exerra <exerra@exerra.xyz>
2022-01-19 17:15:36 +0200

    Fixed DL not working [deploy]

--- app/modules/install.jsx
+++ app/modules/install.jsx
@@ -1,15 +1,13 @@
 export default function installSteps(mod) {
 	return (
-		<form className={"title remix__form"}>
+		<div className={"title remix__form"}>
 			<h1>
 				<span>Install steps</span>
 			</h1>
 			<p>{mod.install}</p>
-			<button className={"idButton"} type="submit" onClick={() => {
-				window.location.href = mod.link
-			}} >
+			<button className={"idButton"} type="submit" onClick={() => { window.location.href = mod.link }}>
 				Download
 			</button>
-		</form>
+		</div>
 	)
 }
