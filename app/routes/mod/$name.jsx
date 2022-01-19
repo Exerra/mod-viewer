03b93318c658d56a7d08fa4b28f05f3d9ead84e1
Exerra <exerra@exerra.xyz>
2022-01-19 16:38:51 +0200

    Mod page meta

--- app/routes/mod/$name.jsx
+++ app/routes/mod/$name.jsx
@@ -8,6 +8,7 @@ export let loader = ({ params }) => {
 			name: "cleanMojaveOutpost",
 			longName: "Clean Mojave Outpost",
 			game: "Fallout: New Vegas",
+			shortDescription: "Removes the cars from the Mojave Outpost.",
 			description: "A simple mod that removes the cars from the Mojave Outpost, making the road much cleaner and nicer. I did this as I think the NCR would at least tidy up their outpost so it would be cleaner and caravans could move in and out without needing to navigate through all those cars.",
 			install: "Use your mod manager of choice or drop in the Data folder",
 			image: "https://cdn.exerra.xyz/games/mods/fnv/exerra/CleanMojaveOutpost/thumbnail.png",
@@ -19,6 +20,7 @@ export let loader = ({ params }) => {
 			name: "easyStartLR",
 			longName: "Easy Start LR",
 			game: "Fallout: New Vegas",
+			shortDescription: "Footlocker with Lonesome Road items",
 			description: "This is a mod which adds a footlocker with LR items. Nothing special.\n\nNote: The multiple dusters isn't a glitch, it is multiple designs of it. The designs are: Regular, Legion, NCR and Old World.",
 			install: "Use your mod manager of choice or drop in the Data folder",
 			image: "https://cdn.exerra.xyz/games/mods/fnv/exerra/EasyStartLR/thumbnail.png",
@@ -37,6 +39,30 @@ export let loader = ({ params }) => {
 
 
 	return data
+}
+
+export let meta = (loader) => {
+	let data = loader.data
+	let metaObj = {}
+
+	let title = data.longName
+	let description = data.shortDescription
+
+	console.log(data.image)
+
+	metaObj = {
+		title: title,
+		description: description,
+		"theme-color": "#EFF5FB",
+		"twitter:card": "summary_large_image",
+		"twitter:image": data.image,
+		"twitter:creator": "@Exerra",
+		"twitter:site": "@Exerra",
+		"twitter:title": title,
+		"twitter:description": description,
+	}
+
+	return metaObj
 }
 
 export default function mod() {
