c12d13ead4e18c91b667780990852eea513e3ae7
Exerra <exerra@exerra.xyz>
2022-01-20 01:31:56 +0200

    susigi bakigi [deploy]

--- app/modules/install.jsx
+++ app/modules/install.jsx
@@ -5,9 +5,18 @@ export default function installSteps(mod) {
 				<span>Install steps</span>
 			</h1>
 			<p>{mod.install}</p>
-			<button className={"idButton"} type="submit" onClick={() => { window.location.href = mod.link }}>
+			<button className={"idButton"} type="submit" onClick={() => { window.location.href = mod.link
+				var x = document.getElementById("snackbar");
+
+				// Add the "show" class to DIV
+				x.className = "show";
+
+				// After 3 seconds, remove the show class from DIV
+				setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+			}}>
 				Download
 			</button>
+			<div id="snackbar">Check downloads folder</div>
 		</div>
 	)
 }
