7ec0a909840551d8080792d5f777a98e12e410c9
Exerra <exerra@exerra.xyz>
2022-01-24 14:29:34 +0200

    morrowind mod time [deploy]

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -20,17 +20,31 @@ export let loader = ({params}) => {
 	]
 
 	let skyrimSE = []
+	let morrowind = [
+		{
+			name: "Ring of Godly Eyes",
+			description: "See through the eyes of the Khajiiti people",
+			image: "https://cdn.exerra.xyz/games/mods/morrowind/exerra/ringOfGodlyEyes/thumbnail.png",
+			link: "/mod/ringOfGodlyEyes",
+			lastUpdated: "24 Jan 2022"
+		}
+	]
 
 
-    return {falloutNV, skyrimSE}
+    return {falloutNV, skyrimSE, morrowind}
 }
 
 export default function Index() {
 	let data = useLoaderData()
 
 	let falloutNVMods = []
+	let morrowindMods = []
 	for (let mod of data.falloutNV) {
 		falloutNVMods.push(modWithThumbnail(mod))
+	}
+
+	for (let mod of data.morrowind) {
+		morrowindMods.push(modWithThumbnail(mod))
 	}
 
 	return (
@@ -41,6 +55,13 @@ export default function Index() {
 			</h1>
 			<div className={"remix__page"}>
 				{falloutNVMods}
+			</div>
+			<br />
+			<h1>
+				Morrowind
+			</h1>
+			<div className={"remix__page"}>
+				{morrowindMods}
 			</div>
 			<br />
 		</div>
